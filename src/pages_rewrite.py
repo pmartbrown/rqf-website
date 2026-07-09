25d923e5453a08391fae5f068c944131a1cf3363
Paul Brown <pmartbrown@gmail.com>
2026-07-09 09:34:45 -0600

    src: relative path rewrite for GitHub Pages

---
+++ src/pages_rewrite.py
@@ -0,0 +1,15 @@
+import os
+PAGES = ['calculators','affiliates','about','faq','privacy','terms','morby-method','echo-method','double-close','emd','hard-money','dscr']
+def rewrite(fp, up):
+    h = open(fp).read()
+    for p in PAGES:
+        h = h.replace('href="/%s/"' % p, 'href="%s%s/"' % (up, p))
+    h = h.replace('href="/#', 'href="%s#' % ('./' if up=='' else up))
+    h = h.replace('href="/"', 'href="%s"' % ('./' if up=='' else up))
+    h = h.replace('src="/assets/', 'src="%sassets/' % up)
+    h = h.replace("url('/assets/", "url('%sassets/" % up)
+    open(fp,'w').write(h)
+rewrite('index.html','')
+for p in PAGES:
+    rewrite(os.path.join(p,'index.html'),'../')
+print('paths made relative for GitHub Pages project site')
